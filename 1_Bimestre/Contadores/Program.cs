52fa9392dab71730d8c83009173e90625acc6c1e
jvelazquez <jonathanvgms@gmail.com>
2024-03-21 10:59:37 -0300

    Add contadores

---
+++ 1_Bimestre/Contadores/Program.cs
@@ -0,0 +1,18 @@
+﻿//Contador
+
+//Incremento
+Console.WriteLine("Ingrese un numero:");
+int numero = Convert.ToInt32(Console.ReadLine());
+int numerosPares = 0; //inicializacion del contador
+if (numero % 2 == 0)
+    numerosPares = numerosPares + 1; //incremento de valor en el contador
+                                     //numerosPares++; incremento en una unidad
+Console.WriteLine(numerosPares);
+
+//decremento
+Console.WriteLine("Ingrese un numero:");
+numero = Convert.ToInt32(Console.ReadLine());
+numero = numero - 1; //decremento en una unidad
+Console.WriteLine(numero);
+numero--; //decremento en una unidad
+Console.WriteLine(numero);
