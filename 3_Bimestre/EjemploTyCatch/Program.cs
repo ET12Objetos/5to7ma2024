5cde649f3c94752ee7eb75f0033702b0514dea55
jvelazquez <jonathanvgms@gmail.com>
2024-09-19 10:22:48 -0300

    Add example try catch

---
+++ 3_Bimestre/EjemploTyCatch/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Ingrese un numero entero: ");
+
+int numeroEntero = 0;
+
+try
+{
+    numeroEntero = Convert.ToInt32(Console.ReadLine());
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("El valor ingresado es incorrecto");
+}
+
+Console.WriteLine(numeroEntero);
